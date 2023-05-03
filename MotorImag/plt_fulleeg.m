c669c004f8986d47cd5123839d0f387438dd2429
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-05-03 15:40:15 +0200

    trying to add path

--- MotorImag/plt_fulleeg.m
+++ MotorImag/plt_fulleeg.m
@@ -1,7 +1,9 @@
 function plt_fulleeg
+    % addpath
+    addpath('/home/tdnguyen/data')
+
     % plot raw EEG signal of vp2
-    addpath('home/tdnguyen/data')
-    load('home/tdnguyen/data/vp2.mat');
+    load('/home/tdnguyen/data/vp2.mat');
     figure1 = figure;
     plot(cnt.x');
     saveas(figure1, 'vp2.fig')
