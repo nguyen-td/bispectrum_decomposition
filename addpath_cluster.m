1e879c82bb1d3eba0048509a9caa4e1302c0e12a
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-07-21 11:31:33 +0200

    does it work without cd ..?

--- addpath_cluster.m
+++ addpath_cluster.m
@@ -1,5 +1,4 @@
 function addpath_cluster
-cd ..
 addpath(genpath('/data/tdnguyen/git_repos/bispectrum_decomposition'))
 addpath('/data/tdnguyen/git_repos/eeglab')
 addpath('/data/tdnguyen/data/imag_data')
