79569fa2fd84976f0b55f28d5cabf35ed6061c8b
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-09-15 13:13:52 +0200

    add file to loop over all subjects

---
+++ main_bsfit_allsubjects.m
@@ -0,0 +1,13 @@
+function main_bsfit_allsubjects(nshuf)
+    subjects = 1:1:37;
+    exclude = [1, 2, 6, 7, 10, 13, 20, 24, 26, 32, 36]; % 26 subjects
+    
+    for isub = 1:length(subjects)
+        if ismember(isub, exclude)
+            continue
+        else
+            fprintf('Subject %d  .............................................................................................. \n', isub)
+            main_bsfit(nshuf, isub)
+        end
+    end
+end
