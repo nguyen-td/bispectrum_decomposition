ead4c8d88d5553852288c00c23da72113cb768ed
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-09-19 11:29:29 +0200

    add back parpoool stuff

--- main_bsfit_allsubjects.m
+++ main_bsfit_allsubjects.m
@@ -3,11 +3,13 @@ function main_bsfit_allsubjects(nshuf)
     exclude = [1, 2, 6, 7, 10, 13, 20, 24, 26, 32, 36]; % 26 subjects
     
     for isub = 1:length(subjects)
+        tic
         if ismember(isub, exclude)
             continue
         else
             fprintf('Subject %d  .............................................................................................. \n', isub)
             main_bsfit(nshuf, isub)
         end
+        toc
     end
 end
