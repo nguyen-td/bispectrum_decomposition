8a63fee9d74ce2040da3b1023377163097c779ca
Hiyeri <48970646+Hiyeri@users.noreply.github.com>
2023-12-20 12:35:17 +0100

    change path names for ramen

--- main.m
+++ main.m
@@ -18,13 +18,13 @@
 function main(nshuf, isub, varargin)
     
     % set directory paths
-%     DIROUT = '/data/tdnguyen/data/p_imag'; % save directory
+    DIROUT = '/data/tdnguyen/data/p_imag'; % save directory
 %     DIROUT = '/data/tdnguyen/data/p_carracer'; % save directory
-    DIROUT = '/Users/nguyentiendung/Desktop/Studium/Charite/Research/Project 1/bispectrum_decomposition/MotorImag/data/figures';
-%     f_path = '/data/tdnguyen/data/imag_data'; % change if necessary
+%     DIROUT = '/Users/nguyentiendung/Desktop/Studium/Charite/Research/Project 1/bispectrum_decomposition/MotorImag/data/figures';
+    f_path = '/data/tdnguyen/data/imag_data'; % change if necessary
 %     f_path = '/Users/nguyentiendung/Desktop/Studium/Charite/Research/Project 1/bispectrum_decomposition/EmergencyBreaking/preprocessing/analysis_output/preprocessing/data';
 %     f_path = '/data/tdnguyen/git_repos/bispectrum_decomposition/EmergencyBreaking/preprocessing/analysis_output/preprocessing/data';
-    f_path = '/Users/nguyentiendung/Desktop/Studium/Charite/Research/Project 1/bispectrum_decomposition/MotorImag/data';
+%     f_path = '/Users/nguyentiendung/Desktop/Studium/Charite/Research/Project 1/bispectrum_decomposition/MotorImag/data';
 
     % setup
     eeglab
