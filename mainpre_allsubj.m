3479e6c00cc153fc0a49812421b45c91a1b4303f
Tien Dung Nguyen <nguyen.tiendung.otten@gmail.com>
2024-04-15 14:35:16 +0200

    add wrapper pipeline

---
+++ mainpre_allsubj.m
@@ -0,0 +1,24 @@
+% Calls main_preanalysis.m for selected subjects with their individual f1. Refer to that function for documentation.
+
+function mainpre_allsubj(n_shuf, varargin)
+    % add path
+    addpath_cluster
+
+    eeglab
+    g = finputcheck(varargin, { ...
+        'alpha'          'float'         { }              0.05;
+        'poolsize'       'integer'       { }              1;
+        'epleng'         'integer'       { }              2;
+        'downsample'     'string'        {'on' 'off'}     'on';
+        'freq_down'      'integer'       { }              125;
+        });
+    if ischar(g), error(g); end
+    
+    subjects = [317, 511, 306, 429, 318, 363, 327, 455, 385, 388];
+    f1 = [11.2, 9.4, 9.1, 9.4, 9.6, 10.4, 11.2, 9.7, 9.4, 9.3];
+
+    for ind_sub = 1:length(subjects)
+        g.f1 = f1(ind_sub);
+        main_preanalysis(n_shuf, subjects(ind_sub), g)
+    end
+end
