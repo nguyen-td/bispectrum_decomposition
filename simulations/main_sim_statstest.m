2f886479cc7e270bd2217ad0f9d7e14786708a0e
Tien Dung Nguyen <nguyen.tiendung.otten@gmail.com>
2024-05-05 16:39:45 +0200

    save files

--- simulations/main_sim_statstest.m
+++ simulations/main_sim_statstest.m
@@ -6,11 +6,12 @@
 %   n_iter - [integer] number of iterations (number of PAC scores to be computed)
 %
 % Optional inputs:
-%   n        - [integer] model order/number of fitted sources, default is 1. Can also be an array of n's, e.g., [3 4 5]
-%   n_univ   - [integer] number of univariate interactions, default is 1
-%   n_biv    - [integer] number of bivariate interactions, default is 0
-%   alpha    - [float] significance level, default is 0.05.
-%   epleng   - [integer] epoch length (see METH toolbox documentation)
+%   n          - [integer] model order/number of fitted sources, default is 1. Can also be an array of n's, e.g., [3 4 5]
+%   n_univ     - [integer] number of univariate interactions, default is 1
+%   n_biv      - [integer] number of bivariate interactions, default is 0
+%   alpha      - [float] significance level, default is 0.05.
+%   epleng     - [integer] epoch length (see METH toolbox documentation)
+%   train_test - ['on' | 'off'] whether A should be fitted on train data and D on test data. If yes, the train-test split is 80-20. Default is 'off'.
 
 function main_sim_statstest(n_shuf, n_iter, varargin)
     
@@ -27,6 +28,7 @@ function main_sim_statstest(n_shuf, n_iter, varargin)
         'epleng'         'integer'       { }                2;
         'alpha'          'float'         { }              0.05;
         'poolsize'       'integer'       { }                1;
+        'train_test'     'string'        { 'on' 'off' }    'off';
         });
     if ischar(g), error(g); end
 
@@ -74,12 +76,18 @@ function main_sim_statstest(n_shuf, n_iter, varargin)
     disp(['Start computing FPR ' int2str(n_iter) ' times...'])
     parpool(g.poolsize)
     fpr_iter = zeros(length(g.n), n_iter); 
+    P_fdr = {};
     tic
     parfor i_iter = 1:n_iter
-        P_fdr = bsfit_stats(signal_sensor, freqinds(1), freqinds(2), g.n, n_shuf, frqs, ...
-            segleng, segshift, epleng, g.alpha, L);
-        fpr = cellfun(@(x) x < g.alpha, P_fdr, 'UniformOutput', false);
-        fpr_iter(:, i_iter) = cell2mat(cellfun(@(x) sum(x, 'all'), fpr, 'UniformOutput', false)) ./ cellfun(@(x) numel(x), P_fdr);
+        if strcmpi(g.train_test, 'off')
+            P_fdr{i_iter} = bsfit_stats(signal_sensor, freqinds(1), freqinds(2), g.n, n_shuf, frqs, ...
+                segleng, segshift, epleng, g.alpha, L);
+        else
+            P_fdr{i_iter} = bsfit_stats(signal_sensor, freqinds(1), freqinds(2), g.n, n_shuf, frqs, ...
+                segleng, segshift, epleng, g.alpha, L, 'train_test', 'on');
+        end
+        fpr = cellfun(@(x) x < g.alpha, P_fdr{i_iter}, 'UniformOutput', false);
+        fpr_iter(:, i_iter) = cell2mat(cellfun(@(x) sum(x, 'all'), fpr, 'UniformOutput', false)) ./ cellfun(@(x) numel(x), P_fdr{i_iter});
     end
     toc
 
@@ -87,11 +95,15 @@ function main_sim_statstest(n_shuf, n_iter, varargin)
     poolobj = gcp('nocreate');
     delete(poolobj);
 
-    % raincloud/half-violin plot on linearly scaled y-axis
-    for i_source = 1:g.n
-        titles = {'Normal', 'Train-Test Split'};
-        colors = [[0 0 0.5]; [0.8 0 0.2]];
-        plot_metrics_raincloud(fpr_iter(i_source, :), colors, 1, 0.2, 'ks', titles, DIROUT, 'name', ['_n' int2str(i_source) f_name]);
-    end
+    % save structs
+    save([DIROUT 'P_fdr_traintest_' g.train_test '.mat'], 'P_fdr', '-v7.3')
+    save([DIROUT 'FPR_traintest_' g.train_test '.mat'], 'fpr_iter', '-v7.3')
+
+%     % raincloud/half-violin plot on linearly scaled y-axis
+%     for i_source = 1:g.n
+%         titles = {'Normal', 'Train-Test Split'};
+%         colors = [[0 0 0.5]; [0.8 0 0.2]];
+%         plot_metrics_raincloud(fpr_iter(i_source, :), colors, 1, 0.2, 'ks', titles, DIROUT, 'name', ['_n' int2str(i_source) f_name]);
+%     end
     
 end
